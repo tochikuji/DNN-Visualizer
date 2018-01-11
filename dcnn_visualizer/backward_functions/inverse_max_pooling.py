c040861a9ad05b1f7527a269546dce41404b72b3
Aiga SUZUKI <ai-suzuki@aist.go.jp>
2018-01-11 14:25:26 +0900

    make code more safer with expect_ndarray
    all blocks which can have mixture of ndarray and Variable were
    completely covered with expect_ndarray

--- dcnn_visualizer/backward_functions/inverse_max_pooling.py
+++ dcnn_visualizer/backward_functions/inverse_max_pooling.py
@@ -2,6 +2,7 @@ import numpy
 import chainer.functions as F
 
 from dcnn_visualizer.roi import InnerROIIterator, ROIGenerator, BaseROIIterator
+from dcnn_visualizer.util import expect_ndarray
 
 
 def sparse_max_unpooling(node, pooled, raw, is_positional):
@@ -23,7 +24,7 @@ def sparse_max_unpooling(node, pooled, raw, is_positional):
                           for pooled_item, raw_item in zip(pooled, raw)])
 
 
-def __sparse_max_unpooling_inner(node, pooled, raw, is_positional):
+def __sparse_max_unpooling_inner(node, pooled_, raw_, is_positional):
     if isinstance(node.ksize, (tuple, list)) and len(set(node.ksize)) != 1:
         raise NotImplementedError('non-square pooling kernel is not supported.')
 
@@ -32,24 +33,26 @@ def __sparse_max_unpooling_inner(node, pooled, raw, is_positional):
     else:
         ksize = int(node.ksize)
 
-    unpooled = numpy.zeros_like(raw, dtype=numpy.float32)
-    roi_iter = InnerROIIterator(unpooled.shape[2], unpooled.shape[1], ksize, node.stride)
+    with expect_ndarray(pooled_) as pooled, expect_ndarray(raw_) as raw:
 
-    for i, (x0, y0, x1, y1) in enumerate(roi_iter):
-        receptive_field = unpooled[:, y0:y1, x0:x1]
-        pooled_y, pooled_x = divmod(i, pooled.shape[2])
+        unpooled = numpy.zeros_like(raw, dtype=numpy.float32)
+        roi_iter = InnerROIIterator(unpooled.shape[2], unpooled.shape[1], ksize, node.stride)
 
-        if is_positional:
-            receptive_field_raw = raw[:, y0:y1, x0:x1]
-            max_locations = __max_location(receptive_field_raw)
+        for i, (x0, y0, x1, y1) in enumerate(roi_iter):
+            receptive_field = unpooled[:, y0:y1, x0:x1]
+            pooled_y, pooled_x = divmod(i, pooled.shape[2])
 
-            for ch_i, (py, px) in enumerate(max_locations):
-                unpooled[ch_i, y0 + py, x0 + px] = pooled[ch_i, pooled_y, pooled_x].data
+            if is_positional:
+                receptive_field_raw = raw[:, y0:y1, x0:x1]
+                max_locations = __max_location(receptive_field_raw)
 
-        else:
-            # is not positional
-            px, py = __center(x0, y0, x1, y1)
-            unpooled[:, y0 + py, x0 + px] = pooled[:, pooled_y, pooled_x].data
+                for ch_i, (py, px) in enumerate(max_locations):
+                    unpooled[ch_i, y0 + py, x0 + px] = pooled[ch_i, pooled_y, pooled_x]
+
+            else:
+                # is not positional
+                px, py = __center(x0, y0, x1, y1)
+                unpooled[:, y0 + py, x0 + px] = pooled[:, pooled_y, pooled_x]
 
     return unpooled
 
@@ -70,6 +73,8 @@ def __center(x0, y0, x1, y1):
     return int((x0 + x1) / 2), int((y0 + y1) / 2)
 
 
-def __max_location(receptive_field_raw):
-    max_locations = [numpy.argwhere(c.data == c.data.max())[0] for c in receptive_field_raw]
+def __max_location(receptive_field_raw_):
+    with expect_ndarray(receptive_field_raw_) as receptive_field_raw:
+        max_locations = [numpy.argwhere(c== c.max())[0] for c in receptive_field_raw]
+
     return tuple([loc.tolist() for loc in max_locations])
