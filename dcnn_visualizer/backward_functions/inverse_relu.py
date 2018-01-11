1005241a7a1b7eb5c615d4b5612fb68866a0f3b6
Aiga SUZUKI <ai-suzuki@aist.go.jp>
2018-01-11 12:22:01 +0900

    fix confusion with Variable and ndarray

--- dcnn_visualizer/backward_functions/inverse_relu.py
+++ dcnn_visualizer/backward_functions/inverse_relu.py
@@ -1,4 +1,6 @@
+import numpy
 import chainer.functions as F
+from dcnn_visualizer.util import expect_ndarray
 
 
 def inverse_relu_anew(node, rectified, raw):
@@ -8,10 +10,13 @@ def inverse_relu_anew(node, rectified, raw):
 
 
 def inverse_relu_locational(node, rectified, raw):
-    positives = rectified * (raw > 0)
+    with expect_ndarray(rectified) as re_data:
+        with expect_ndarray(raw) as raw_data:
+            positives = re_data * (raw_data > 0)
 
     return positives
 
 
 def relu(x):
-    return x * (x > 0)
+    with expect_ndarray(x) as data:
+        return data * (data > 0)
